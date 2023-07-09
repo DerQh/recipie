a911bc7fb606f48906ed5fc97881b4f63d0cfba6
DerQh <sanafredrick@gmail.com>
2023-07-09 20:30:28 +0300

    js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,21 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipie = async function () {
+  const apiKey = "fe6c0263-055e-4cb7-a199-cd527b7f80ad";
+
+  try {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886?key=${apiKey}`
+    );
+    const data = await response.json();
+
+    if(!response.ok) throw new Error(`${data.mesaage} (${response.status})`)
+    console.log(response,data);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipie();
