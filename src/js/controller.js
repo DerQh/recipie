e5e5f4c1a957030cc8610fdb0544d5a25463ce36
DerQh <sanafredrick@gmail.com>
2023-07-14 15:14:33 +0300

    1401

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,6 @@
 import * as model from "./model.js";
+import recipieView from "./views/recipieView.js";
 
-// import icons from '../img/icons.svg' - parcel 1
-import icons from "url:../img/icons.svg";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
@@ -18,139 +17,29 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-const renderSpinner = function (parentElement) {
-  const markup = `
-       <div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div> 
-        `;
-  parentElement.insertAdjacentHTML("afterbegin", markup);
-};
 
-const showRecipie = async function () {
+const controlRecipies = async function () {
   try {
     // const id = window.location.hash.slice(1);
     const id = "5ed6604591c37cdc054bc886";
     console.log(id);
 
     if (!id) return; // this is a guard clause
-    renderSpinner(recipeContainer);
+    recipieView.renderSpinner();
 
     // loading data
     await model.loadRecipe(id); //async funciton - returns a promise that we have to handle
 
     // render data
-    const markup = `
-      <figure class="recipe__fig">
-          <img src="${recipie.image}" alt="${
-      recipie.title
-    }" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipie.title}</span>
-          </h1>
-        </figure>
-
-    <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${
-              recipie.cookingTime
-            }</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${
-              recipie.servings
-            }</span>
-            <span class="recipe__info-text">servings</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-          ${recipie.ingredients
-            .map((ing) => {
-              return `
-             <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>
-            `;
-            })
-            .join("")}
-
-          </ul>
-        </div>
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-              recipie.publisher
-            }</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${recipie.sourceUrl}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>
-        `;
-    recipeContainer.innerHTML = "";
-    recipeContainer.insertAdjacentHTML("afterbegin", markup);
+    recipieView.render(model.state.recipie);
   } catch (err) {
     alert(err);
   }
 };
 
-// window.addEventListener("hashchange", showRecipie);
-// window.addEventListener("load", showRecipie);
+// window.addEventListener("hashchange", controlRecipies);
+// window.addEventListener("load", controlRecipies);
 
 ["hashchange", "load"].forEach((event) =>
-  window.addEventListener(event, showRecipie)
+  window.addEventListener(event, controlRecipies)
 );
