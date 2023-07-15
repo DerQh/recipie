9a5f611ecb35714fe0aa0a40cb3143bbb2ac5619
DerQh <sanafredrick@gmail.com>
2023-07-15 17:50:00 +0300

    15

--- src/js/views/recipieView.js
+++ src/js/views/recipieView.js
@@ -1,5 +1,7 @@
 // import icons from '../img/icons.svg' - parcel 1
 import icons from "url:../../img/icons.svg";
+import { Fraction } from "fractional";
+// console.log(Fraction);
 
 class RecipieView {
   #parentEl = document.querySelector(".recipe");
@@ -7,8 +9,8 @@ class RecipieView {
 
   render(data) {
     this.#data = data;
-    const markup = this._generateMarkup();
-    // this.#clear;
+    const markup = this.#generateMarkup();
+    this.#clear;
     this.#parentEl.insertAdjacentHTML("afterbegin", markup);
   }
 
@@ -27,7 +29,7 @@ class RecipieView {
     this.#parentEl.insertAdjacentHTML("afterbegin", markup);
   };
 
-  _generateMarkup() {
+  #generateMarkup() {
     return `
       <figure class="recipe__fig">
           <img src="${this.#data.image}" alt="${
@@ -86,22 +88,7 @@ class RecipieView {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
-          ${this.#data.ingredients
-            .map((ing) => {
-              return `
-             <li class="recipe__ingredient">
-              <svg class="recipe__icon">
-                <use href="${icons}#icon-check"></use>
-              </svg>
-              <div class="recipe__quantity">${ing.quantity}</div>
-              <div class="recipe__description">
-                <span class="recipe__unit">${ing.unit}</span>
-                ${ing.description}
-              </div>
-            </li>
-            `;
-            })
-            .join("")}
+          ${this.#data.ingredients.map(this.#generateMarkupIngredient).join("")}
 
           </ul>
         </div>
@@ -126,6 +113,23 @@ class RecipieView {
             </svg>
           </a>
         </div>
+        `;
+  }
+
+  #generateMarkupIngredient(ing) {
+    return `
+             <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="${icons}#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">${
+                ing.quantity ? new Fraction(ing.quantity).toString() : ""
+              }</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">${ing.unit}</span>
+                ${ing.description}
+              </div>
+            </li>
         `;
   }
 }
